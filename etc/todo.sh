cc01fcedc97a816bc82e59545c05ddfa751a708d
JordanIschard <jordan_is@hotmail.fr>
2024-11-25 14:06:11 +0100

    fix todo script

--- etc/todo.sh
+++ etc/todo.sh
@@ -2,8 +2,8 @@
 
 echo "------------------------------------------"
 echo "Statements to prove in files: "
-find . -name "*.v" -exec grep -le [Aa]dmit {} \;
+find theories -name "*.v" -exec grep -le [Aa]dmit {} \;
 echo "------------------------------------------"
 echo -n "To prove: "
-echo $(find . -name "*.v" -exec grep -He [Aa]dmit {} \; | wc -l)
+echo $(find theories -name "*.v" -exec grep -He [Aa]dmit {} \; | wc -l)
 echo "------------------------------------------"
